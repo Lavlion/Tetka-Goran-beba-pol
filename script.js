c008de448e92a74edca3f8553e92159fff8f5597
Lavlion <lavart1401@gmail.com>
2025-04-12 12:47:30 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+const button = document.querySelector("#button");
+const canvas = document.querySelector("#confetti");
+
+const jsConfetti = new JSConfetti();
+
+button.addEventListener("click", () => {
+  jsConfetti
+    .addConfetti({
+      emojis: ["👦"]
+    })
+    .then(() => jsConfetti.addConfetti());
+});
